e9e9b43d395f68415a1b498884f638aa959ed0e6
DPach <63697168+Prads-ai@users.noreply.github.com>
2023-02-15 14:34:26 -0500

    Add files via upload

---
+++ Servlets/AddToCartServlet.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import BusinessObject.Cart;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author pach3
+ */
+@WebServlet(name = "AddToCartServlet", urlPatterns = {"/add-to-cart"})
+public class AddToCartServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            HttpSession cartId = request.getSession();
+            List<Cart> cartList = new ArrayList<>();
+            
+            int id = Integer.parseInt(request.getParameter("id"));
+            Cart cm = new Cart();
+            cm.getId();
+            cm.setQuantity(1);
+            cm = cm.getCartId(id);
+           
+            HttpSession session = request.getSession();
+            List<Cart> cart_list = (ArrayList<Cart>) session.getAttribute("cart-list");
+            
+            if (cart_list == null) {
+                cm.setStatus("READY");
+                cartList.add(cm);
+                session.setAttribute("cart-list", cartList);
+                response.sendRedirect("index.jsp");
+            } else {
+                cartList = cart_list;
+                boolean exist = false;
+
+                for (Cart c : cart_list) {
+                    if (c.getId() == id) {
+                        exist = true;
+                        out.print("<h3 style='color: crimson; text-align: center;'>already exist in your Cart. <a href='cart.jsp'> Go to Cart page </a>");
+                    }
+                }
+                if (!exist) {
+                    cm.setStatus("READY");
+                    cartList.add(cm);
+                    response.sendRedirect("index.jsp");
+                }
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
