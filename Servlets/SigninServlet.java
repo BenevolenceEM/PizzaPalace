2d04044d9b8d075fcc79646b2ff35ed1a8fa2ffa
BenevolenceEM <maliksofia58@gmail.com>
2023-03-07 00:05:37 -0500

    Login

---
+++ Servlets/SigninServlet.java
@@ -0,0 +1,113 @@
+/*
+Benevolence Ed-Malik
+Pradsley D'Haiti
+ */
+
+import BusinessObject.Customer;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet(urlPatterns = {"/SigninServlet"})
+public class SigninServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        PrintWriter out = response.getWriter();
+        
+        String email, password;
+        email = request.getParameter("email");
+        password = request.getParameter("password");
+        
+        Customer c1 = new Customer();
+        c1.selectEmail(email);
+        System.out.println(c1.getEmail());
+        System.out.println(c1.getPassword());
+        
+        HttpSession ses1;
+        ses1 = request.getSession();
+        ses1.setAttribute("c1", c1);
+        System.out.println("Customer has been added to Session");
+        c1.display();
+        
+        String EMAIL, PASS;
+        EMAIL = c1.getEmail();
+        PASS = c1.getPassword();
+        System.out.println(EMAIL);
+        System.out.println(PASS);
+        
+        boolean empty = (email.equals("") && password.equals(""));
+        boolean valid = EMAIL.equals(email) && PASS.equals(password);
+        System.out.println(empty);
+        System.out.println(valid);
+        
+        if(empty) {
+            response.sendRedirect("Signin.jsp?alert=empty");
+        }
+        else if(valid) {
+            RequestDispatcher rd = request.getRequestDispatcher("ViewAccount.jsp");
+            rd.forward(request, response);
+        }
+        else {
+            response.sendRedirect("Signin.jsp?alert1=invalid");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ Signin.jsp
@@ -0,0 +1,77 @@
+<%--
+    Benevolence Ed-Malik
+    Pradsley D'Haiti
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Pizza Palace Rewards - Sign In</title>
+        <link rel="stylesheet" href="main.css">
+        <link rel="icon" type="image/png" sizes="32x32" href="images/logo.png">
+    </head>
+    <body>
+        <header>
+            <a href="index.html" class="logo">
+                <img src="images/image.png" alt="logo">
+            </a>
+            <nav>
+                <a href="" class="promos">Promos</a>
+                <li class="menu-dropdown">
+                    <a href="" class="menu">Menu</a>
+                    <div class="menu-content">
+                        <a href="">Pizza</a>
+                        <a href="">Wings</a>
+                        <a href="">Pasta</a>
+                        <a href="">Sides</a>
+                        <a href="">Desserts</a>
+                        <a href="">Salads</a>
+                        <a href="">Drinks</a>
+                        <a href="">Dips</a>
+                    </div>
+                </li>
+                <a href="#" class="account">Pizza Palace Rewards</a>
+            </nav>
+        </header>
+
+        <h3 style="text-align: center">Don't have one? 
+            <a href="#" style="color: #cf641c; text-decoration: underline">
+            Join the rewards program now</a>
+        </h3>
+        
+        <div class="container">
+            <form action="SigninServlet">
+                <br>Email<br> <input type="text" id="email" name="email"> <br>
+                Password<br> <input type="text" id="password" name="password">
+                <br><br>
+                <a href="#" style="color: #cf641c; text-decoration: underline">
+                FORGOT PASSWORD?</a><br><br>
+                
+                <input type="submit" value="SIGN IN">
+                <input type="reset" value="CLEAR">
+            </form>
+        </div>
+        <%
+            String alert = request.getParameter("alert");
+            if ("empty".equals(alert)) {
+        %>
+        <script>
+            alert("Empty login. Please input login credentials");
+        </script>
+        <%
+            }
+        %>
+        <%
+            String alert1 = request.getParameter("alert1");
+            if ("invalid".equals(alert1)) {
+        %>
+        <script>
+            alert("Invalid Login. Please try again");
+        </script>
+        <%
+            }
+        %>
+    </body>
+</html>
