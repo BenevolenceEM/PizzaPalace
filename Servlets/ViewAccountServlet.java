82e38020d2ade6169c1e51f0e19abdb32e515fae
BenevolenceEM <maliksofia58@gmail.com>
2023-03-12 01:48:52 -0500

    Create ViewAccountServlet.java

---
+++ Servlets/ViewAccountServlet.java
@@ -0,0 +1,100 @@
+/*
+Benevolence Ed-Malik
+Pradsley D'Haiti
+ */
+
+import BusinessObject.Customer;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet(urlPatterns = {"/ViewAccountServlet"})
+public class ViewAccountServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        HttpSession ses1;
+        ses1 = request.getSession();
+            
+        Customer c1 = (Customer)ses1.getAttribute("c1");
+        out.println(c1.getPassword());
+        out.println(c1.getFirstname());
+        out.println(c1.getLastname());
+        out.println(c1.getPhone());
+            
+        String Password = request.getParameter("password");
+        String Firstname = request.getParameter("fname");
+        String Lastname = request.getParameter("lname");
+        String Phone = request.getParameter("phone");
+            
+        c1.setPassword(Password);
+        c1.setFirstname(Firstname);
+        c1.setLastname(Lastname);
+        c1.setPhone(Phone);
+            
+        c1.updateDB();
+        
+        RequestDispatcher rd = request.getRequestDispatcher("ViewAccount.jsp");
+        rd.forward(request, response);
+    }
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
